fec3de3583fb7cb14da67ee122eb9ae0e11e9233
Icey Grave <pete@ixiaconsultancy.com>
2020-03-28 10:49:28 +0000

    Your Message

--- src/site/_data/prod/sheet.js
+++ src/site/_data/prod/sheet.js
@@ -11,34 +11,44 @@ console.log(googleSheetUrl);
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
-
-    console.log(`Requesting data from ${googleSheetUrl}`);
-
+    console.log(`Requesting content from ${googleSheetUrl}`);
     axios.get(googleSheetUrl)
       .then(response => {
-
         // massage the data from the Google Sheets API into
         // a shape that will more convenient for us in our SSG.
         var data = {
-          "East": [],
-          "West": []
+          "content": []
         };
         response.data.feed.entry.forEach(item => {
-          data[item.gsx$conference.$t].push({
-            "name": item.gsx$name.$t,
-            "team": item.gsx$team.$t
+          data.content.push({
+            "header": item.gsx$header.$t,
+            "header2": item.gsx$header2.$t,
+            "body": item.gsx$body.$t,
+            "body2": item.gsx$body2.$t,
+            "body3":  item.gsx$body3.$t,
+            "body4": item.gsx$body4.$t,
+            "body5": item.gsx$body5.$t,
+            "body6":  item.gsx$body6.$t,
+            "body7": item.gsx$body7.$t,
+            "body8": item.gsx$body8.$t,
+            "body9":  item.gsx$body9.$t,
+            "body10": item.gsx$body10.$t,
+            "body11": item.gsx$body11.$t,
+            "body12":  item.gsx$body12.$t,
+            "body13": item.gsx$body13.$t,
+            "body14": item.gsx$body14.$t,
+            "body15":  item.gsx$body15.$t,
+            "body16": item.gsx$body16.$t,
+            "body17": item.gsx$body17.$t,
+            
           })
         });
-
         // stash the data locally for developing without
         // needing to hit the API each time.
         seed(JSON.stringify(data), `${__dirname}/../dev/sheet.json`);
-
         // resolve the promise and return the data
         resolve(data);
-
       })
-
       // uh-oh. Handle any errrors we might encounter
       .catch(error => {
         console.log('Error :', error);
